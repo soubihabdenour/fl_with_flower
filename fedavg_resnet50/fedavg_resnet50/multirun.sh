6d43ee7394d2498e9f14c2a0337472e723e55df6
abdenour soubih <soubih.abdenour@yahoo.com>
2024-10-01 12:47:32 +0900

    fedavg_resnet50

--- fedavg_resnet50/fedavg_resnet50/multirun.sh
+++ fedavg_resnet50/fedavg_resnet50/multirun.sh
@@ -4,7 +4,7 @@ cd ..
 # Experiment 2
 echo "Starting Experiment 3"
 
-python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=7 model.num_classes=9
+#python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=7 model.num_classes=9
 
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
@@ -13,7 +13,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Starting Experiment 2"
-python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=4 model.num_classes=9
+#python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=4 model.num_classes=9
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
   echo "Experiment 2 failed!"
@@ -22,7 +22,7 @@ fi
 # Experiment 1
 echo "Starting Experiment 1 "
 
-python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=2 model.num_classes=9
+#python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=2 model.num_classes=9
 
 # Check if Experiment 1 succeeded
 if [ $? -ne 0 ]; then
@@ -33,7 +33,7 @@ fi
 # Experiment 2
 echo "Starting Experiment 3"
 
-python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.9 model.num_classes=9
+#python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.9 model.num_classes=9
 
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
@@ -42,7 +42,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Starting Experiment 2"
-python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.3 model.num_classes=9
+#python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.3 model.num_classes=9
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
   echo "Experiment 2 failed!"
@@ -51,7 +51,7 @@ fi
 # Experiment 1
 echo "Starting Experiment 1 "
 
-python -m fedavg_inception.main dataset.subset="pathmnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.1 model.num_classes=9
+python -m fedavg_resnet50.main dataset.subset="pathmnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.1 model.num_classes=9
 
 # Check if Experiment 1 succeeded
 if [ $? -ne 0 ]; then
@@ -123,7 +123,7 @@ fi
 # Experiment 2
 echo "Starting Experiment 3"
 
-python -m fedavg_inception.main dataset.subset="tissuemnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=7 model.num_classes=8
+python -m fedavg_resnet50.main dataset.subset="tissuemnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=7 model.num_classes=8
 
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
@@ -132,7 +132,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Starting Experiment 2"
-python -m fedavg_inception.main dataset.subset="tissuemnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=4 model.num_classes=8
+python -m fedavg_resnet50.main dataset.subset="tissuemnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=4 model.num_classes=8
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
   echo "Experiment 2 failed!"
@@ -141,7 +141,7 @@ fi
 # Experiment 1
 echo "Starting Experiment 1 "
 
-python -m fedavg_inception.main dataset.subset="tissuemnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=2 model.num_classes=8
+python -m fedavg_resnet50.main dataset.subset="tissuemnist" dataset.partitioner.name="PathologicalPartitioner" dataset.partitioner.num_classes_per_partition=2 model.num_classes=8
 
 # Check if Experiment 1 succeeded
 if [ $? -ne 0 ]; then
@@ -152,7 +152,7 @@ fi
 # Experiment 2
 echo "Starting Experiment 3"
 
-python -m fedavg_inception.main dataset.subset="tissuemnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.9 model.num_classes=8
+python -m fedavg_resnet50.main dataset.subset="tissuemnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.9 model.num_classes=8
 
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
@@ -161,7 +161,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Starting Experiment 2"
-python -m fedavg_inception.main dataset.subset="tissuemnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.3 model.num_classes=8
+python -m fedavg_resnet50.main dataset.subset="tissuemnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.3 model.num_classes=8
 # Check if Experiment 2 succeeded
 if [ $? -ne 0 ]; then
   echo "Experiment 2 failed!"
@@ -170,7 +170,7 @@ fi
 # Experiment 1
 echo "Starting Experiment 1 "
 
-python -m fedavg_inception.main dataset.subset="tissuemnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.1 model.num_classes=8
+python -m fedavg_resnet50.main dataset.subset="tissuemnist" dataset.partitioner.name="DirichletPartitioner" dataset.partitioner.alpha=0.1 model.num_classes=8
 
 # Check if Experiment 1 succeeded
 if [ $? -ne 0 ]; then
